baf9c10a050ec1d681ca43cfed21d3ada63d6d8b
Rafael Ferreira <rafael.ferreira@br.experian.com>
2021-05-20 03:07:53 -0300

    Criado layout da Tela de Boas Vindas e navigation que leva para ela

--- app/src/main/java/com/udacity/shoestore/FragLogin.kt
+++ app/src/main/java/com/udacity/shoestore/FragLogin.kt
@@ -6,16 +6,31 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 class FragLogin : Fragment() {
 
+    private lateinit var binding: FragmentLoginBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val binding: FragmentLoginBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            mbtLogin.setOnClickListener {
+                directionToFragWelcome()
+            }
+            mbtLoginExistingAccount.setOnClickListener {
+                directionToFragWelcome()
+            }
+        }
+    }
+
+    private fun directionToFragWelcome() {
+        findNavController().navigate(FragLoginDirections.actionFragLoginToFragWelcome())
     }
 }
---
+++ app/src/main/java/com/udacity/shoestore/FragWelcome.kt
@@ -0,0 +1,21 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
+
+class FragWelcome : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val binding: FragmentWelcomeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
