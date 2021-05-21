77a82987dc3d08eddc30404be3a45cda2f9b77ee
Rafael Ferreira <rafael.ferreira@br.experian.com>
2021-05-21 02:13:02 -0300

    Criado layout da tela de detalhes do sapato e navigation que leva para ela

--- app/src/main/java/com/udacity/shoestore/FragShoeDetail.kt
+++ app/src/main/java/com/udacity/shoestore/FragShoeDetail.kt
@@ -6,24 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.udacity.shoestore.databinding.FragmentShoeListBinding
+import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
 
-class FragShoeList : Fragment() {
+class FragShoeDetail : Fragment() {
 
-    private lateinit var binding: FragmentShoeListBinding
+    private lateinit var binding: FragmentShoeDetailBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_list, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_detail, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.run {
-            fabInstruction.setOnClickListener {
-                findNavController().navigate(FragShoeListDirections.actionFragShoeListToFragShoeDetail())
-            }
+
         }
     }
 }
